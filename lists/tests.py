f68eb4b3f4aa39214433ba499132b24d48a22edc
Robin Rheem <roborheems@gmail.com>
2018-09-23 15:44:34 +0900

    Pass unit test and create url mapping with dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
@@ -6,6 +8,7 @@ class SmokeTest(TestCase):
     The `TestCase` class is actually an extended verison of unittest.TestCase.
     It has Django specific features. Let's find out later.
     """
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
