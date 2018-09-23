19c048fe38b91d5f3267198cf7d8b207a0369a29
Robin Rheem <roborheems@gmail.com>
2018-09-23 14:24:20 +0900

    Write first failing test for lists app

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SmokeTest(TestCase):
+    """
+    The `TestCase` class is actually an extended verison of unittest.TestCase.
+    It has Django specific features. Let's find out later.
+    """
+    def test_bad_maths(self):
+        self.assertEqual(1 + 1, 3)
+
