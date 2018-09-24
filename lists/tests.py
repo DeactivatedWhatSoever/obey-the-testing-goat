37c3056d6721ba286f522cc3baf5915e8f614989
Robin Rheem <roborheems@gmail.com>
2018-09-24 12:25:00 +0900

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from lists.views import home_page
-
 
 
 class SmokeTest(TestCase):
@@ -11,15 +6,7 @@ class SmokeTest(TestCase):
     The `TestCase` class is actually an extended verison of unittest.TestCase.
     It has Django specific features. Let's find out later.
     """
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
